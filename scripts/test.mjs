c42992f4376c1a3e9d47a82894ebe924c6adeae6
bddjr <bddjr08@outlook.com>
2026-08-22 13:35:40 +0800

    test

---
+++ scripts/test.mjs
@@ -0,0 +1,52 @@
+import sb3Crypto from "@openccw/sb3-crypto";
+import assert from "node:assert";
+import fs from 'node:fs'
+import path from "node:path";
+
+const inputDir = 'testdata/input'
+const outputDir = 'testdata/output'
+const outputDecryptDir = outputDir + '/decrypt'
+const outputEncryptDir = outputDir + '/encrypt'
+
+fs.rmSync(outputDir, { recursive: true, force: true })
+fs.mkdirSync(outputDir)
+fs.mkdirSync(outputDecryptDir)
+fs.mkdirSync(outputEncryptDir)
+
+for (const inputFileStat of fs.readdirSync(inputDir, { withFileTypes: true })) {
+    const inputFileName = inputFileStat.name;
+    if (!inputFileStat.isFile() || !inputFileName.endsWith('.sb3'))
+        continue;
+
+    const outputEncryptFileName = inputFileName.replace(/\.sb3$/, '.json')
+
+    const inputFilePath = path.join(inputDir, inputFileName)
+    const outputDecryptFilePath = path.join(outputDecryptDir, outputEncryptFileName)
+    const outputEncryptFilePath = path.join(outputEncryptDir, inputFileName)
+
+    console.log('decrypt', inputFilePath)
+    const inputFileData = fs.readFileSync(inputFilePath)
+    const decryptedProjectJson = await sb3Crypto.decrypt.decryptToProjectJson(inputFileData, inputFileName)
+    fs.writeFileSync(outputDecryptFilePath, decryptedProjectJson)
+
+    console.log('encrypt', outputDecryptFilePath)
+    const encryptedSb3 = await sb3Crypto.encrypt.encryptProjectJson(decryptedProjectJson, inputFileName)
+    fs.writeFileSync(outputEncryptFilePath, encryptedSb3)
+
+    console.log('decrypt-again', outputEncryptFilePath)
+    const decryptedProjectJson2 = await sb3Crypto.decrypt.decryptToProjectJson(Buffer.from(encryptedSb3), inputFileName)
+
+    console.log('compare json')
+    assert.strictEqual(
+        decryptedProjectJson,
+        decryptedProjectJson2,
+        `not equal`
+    )
+
+    console.log('parse json')
+    JSON.parse(decryptedProjectJson)
+
+    console.log()
+}
+
+console.log('ok')
