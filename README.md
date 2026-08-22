6b8a62e0221c6dceec875fbf4fdabbde459ba563
bddjr <bddjr08@outlook.com>
2026-08-22 10:48:35 +0800

    export default

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 基于 Web Crypto API + JSZip 实现 CCW sb3 文件的加密和解密。
+
+## 安装
+
+### npm
+
+```
+npm i @openccw/sb3-crypto
+```
+
+```js
+import sb3Crypto from "@openccw/sb3-crypto";
+```
+
+### 其它包管理器
+
+你也可以使用其它包管理器（例如 `pnpm` 或 `yarn`）代替 `npm` 。
+
+## 使用
+
+```js
+// TODO...
+```
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-export * from './decrypt.js'
-export * from './encrypt.js'
+import * as encrypt from './encrypt.js'
+import * as decrypt from './decrypt.js'
+
+const sb3Crypto = {
+    encrypt,
+    decrypt
+}
+
+export {
+    sb3Crypto as default,
+    encrypt,
+    decrypt
+}
