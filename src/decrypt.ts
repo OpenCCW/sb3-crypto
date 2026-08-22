6a555d4ed50d8998684f38f10fa7f86da663f49f
bddjr <bddjr08@outlook.com>
2026-08-22 11:42:46 +0800

    failed to decrypt sb3: data must have at least 8 bytes

--- src/decrypt.ts
+++ src/decrypt.ts
@@ -7,6 +7,9 @@ import { cryptoTransform } from "./crypto-transform.js";
 const _PK = Uint8Array.of(80, 75, 3, 4, 10, 0, 0, 0); // PK ....
 
 const _decryptSb3 = async (data: Uint8Array, fileName: string): Promise<Uint8Array> => {
+    if (data.length < 8)
+        throw Error(`failed to decrypt sb3: data must have at least 8 bytes`);
+
     // 若 data[2] 不是可见的 ASCII 字符，则 data 是二进制数据。
     switch (data[0] << 16 | data[1] << 8 | data[2]) {
         case 0x504b03:
@@ -18,8 +21,6 @@ const _decryptSb3 = async (data: Uint8Array, fileName: string): Promise<Uint8Arr
             // [55, 122, 188, 175, 9, 5, 2, 7]
             // 保持和输入数据一样的长度（前 8 字节将被签名覆盖），
             // 但必须保证长度不少于 8 字节，否则放不下签名。
-            if (data.length < 8)
-                return _PK.slice();
             // 为了防止 Buffer 类型导致意外情况，这里不能用 slice 。
             const out = Uint8Array.from(data);
             out.set(_PK, 0);
