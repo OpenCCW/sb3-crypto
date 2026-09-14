ed7e57fa59f958c9ffb46270d343844028d62a25
bddjr <bddjr08@outlook.com>
2026-09-14 23:44:03 +0800

    优化keyBytes

--- src/crypto-transform.ts
+++ src/crypto-transform.ts
@@ -1,3 +1,8 @@
+// 逆向代码：
+//   在 vendor~main 搜 .AES.
+//   在 vendor~main 搜 .split(",")))
+//   在 vendor~main 搜 .endsWith(".sb3")
+
 import parseBase64 from "parse-base64-like-crypto-js";
 
 export const cryptoTransform = async (
@@ -5,8 +10,18 @@ export const cryptoTransform = async (
     fileName: string,
     data: BufferSource
 ): Promise<ArrayBuffer> => {
-    let keyBytes = parseBase64("KzdnFCBRvq3" + fileName).subarray(0, 32);
-    if (keyBytes.length < 32) {
+    // fileName 去掉前面的路径，去掉 ".sb3" 扩展名
+    let i = fileName.lastIndexOf('/')
+    if (i !== -1)
+        fileName = fileName.slice(i)
+    if (fileName.endsWith(".sb3"))
+        fileName = fileName.slice(0, -4)
+
+    let keyBytes = parseBase64("KzdnFCBRvq3" + fileName)
+
+    if (keyBytes.length > 32) {
+        keyBytes = keyBytes.subarray(0, 32)
+    } else if (keyBytes.length < 32) {
         const b = new Uint8Array(32)
         b.set(keyBytes)
         keyBytes = b
