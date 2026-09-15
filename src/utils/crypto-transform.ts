3d97d6efd78b0f90487440ddb155de6229d41a03
bddjr <bddjr08@outlook.com>
2026-09-15 21:31:40 +0800

    modified: src/utils/crypto-transform.ts

--- src/utils/crypto-transform.ts
+++ src/utils/crypto-transform.ts
@@ -10,20 +10,15 @@ export const cryptoTransform = async (
     fileName: string,
     data: BufferSource
 ): Promise<ArrayBuffer> => {
-    // fileName 去掉 ".sb3" 扩展名，去掉前面的路径
-    a: {
-        const hasExt = fileName.endsWith(".sb3")
-        const end = fileName.length - (hasExt ? 4 : 0)
-        for (let i = end; i;) {
-            switch (fileName.charCodeAt(--i)) {
-                case 47: // '/'
-                case 92: // '\'
-                    fileName = fileName.slice(i + 1, end)
-                    break a
-            }
-        }
-        if (hasExt) fileName = fileName.slice(0, end)
-    }
+    // fileName 去掉网址参数，去掉 ".sb3" 扩展名，去掉前面的路径
+    let i = fileName.indexOf('?')
+    if (i !== -1)
+        fileName = fileName.slice(0, i)
+    if (fileName.endsWith(".sb3"))
+        fileName = fileName.slice(0, -4)
+    i = Math.max(fileName.lastIndexOf('/'), fileName.lastIndexOf('\\'))
+    if (i !== -1)
+        fileName = fileName.slice(i + 1)
 
     const keyBytes = parseBase64("KzdnFCBRvq3" + fileName, false, 32)
 
