bdeff59b58e8ca2228c64a0383bc6b29b8436cc1
bddjr <bddjr08@outlook.com>
2026-09-16 11:29:33 +0800

    modified: src/utils/comma-separated-bytes.ts

--- src/utils/comma-separated-bytes.ts
+++ src/utils/comma-separated-bytes.ts
@@ -6,16 +6,16 @@
 
 let _decoder: TextDecoder | undefined;
 
-/** 快速模拟 `new Uint8Array(new TextDecoder().decode(u8a).split(","))` */
-export const csbSplit = (input: Uint8Array): Uint8Array<ArrayBuffer> => {
-    const inputLen = input.length
+/** 快速模拟 `new Uint8Array(new TextDecoder().decode(csb).split(","))` */
+export const csbSplit = (csb: Uint8Array): Uint8Array<ArrayBuffer> => {
+    const inputLen = csb.length
     // "".split(",") -> [""] -> [0]
     if (!inputLen) return new Uint8Array(1)
 
     let outLen = 1
     // count ','
     for (let ii = 0; ii < inputLen; ii++) {
-        if (input[ii] === 44) outLen++
+        if (csb[ii] === 44) outLen++
     }
 
     const out = new Uint8Array(outLen);
@@ -23,7 +23,7 @@ export const csbSplit = (input: Uint8Array): Uint8Array<ArrayBuffer> => {
     let oi = 0
     let value = 0
     for (let i = 0, start = 0; i < inputLen; i++) {
-        const c = input[i]
+        const c = csb[i]
         if (c >= 48 && c <= 57) {
             // [快路径] 纯十进制累加，每个字符仅访问一次
             value = c - 48 + value * 10
@@ -36,12 +36,12 @@ export const csbSplit = (input: Uint8Array): Uint8Array<ArrayBuffer> => {
             // [慢路径] 发现非数字字符，子循环快速扫描到本 token 结尾。
             // 正常的文件不会触发慢路径，但为了保证解析结果一致，所以保留了慢路径。
             let end = i + 1
-            while (end < inputLen && input[end] !== 44) {
+            while (end < inputLen && csb[end] !== 44) {
                 end++
             }
             // 局部回退解析
             _decoder ??= new TextDecoder;
-            out[oi++] = +_decoder.decode(input.subarray(start, end))
+            out[oi++] = +_decoder.decode(csb.subarray(start, end))
             // 指针跳过整个慢路径 token（本轮结束后 i++ 会正好跳过逗号，落在下一个 token 的首字符）
             i = end
             start = end + 1
@@ -54,21 +54,21 @@ export const csbSplit = (input: Uint8Array): Uint8Array<ArrayBuffer> => {
 
 let _DIGIT_LENS: Uint8Array<ArrayBuffer> | undefined;
 
-/** 快速模拟 `new TextEncoder().encode(u8a.join())` */
-export const csbJoin = (input: Uint8Array): Uint8Array<ArrayBuffer> => {
-    const inputLen = input.length
+/** 快速模拟 `new TextEncoder().encode(bytes.join())` */
+export const csbJoin = (bytes: Uint8Array): Uint8Array<ArrayBuffer> => {
+    const inputLen = bytes.length
     if (!inputLen) return new Uint8Array;
 
     _DIGIT_LENS ??= new Uint8Array(256).fill(1, 0, 10).fill(2, 10, 100).fill(3, 100)
 
     let outLen = inputLen - 1
     for (let ii = 0; ii < inputLen; ii++) {
-        outLen += _DIGIT_LENS[input[ii]]
+        outLen += _DIGIT_LENS[bytes[ii]]
     }
 
     const out = new Uint8Array(outLen)
     for (let ii = 0, oi = 0; ii < inputLen; ii++) {
-        const v = input[ii]
+        const v = bytes[ii]
         if (v > 9) {
             // 10-255
             const d10 = 0 | (v / 10)
